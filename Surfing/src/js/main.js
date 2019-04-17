7cab0cd24482871e0bd69ba751e2e47cbde9c081
kwalx <alkrivulya@gmail.com>
2019-04-18 02:02:25 +0300

    video added to INFO section

--- Surfing/src/js/main.js
+++ Surfing/src/js/main.js
@@ -42,4 +42,60 @@ $(document).ready(function() {
       }
     });
   })();
+
+  /* Video (Info section) */
+  (function() {
+    function findVideos() {
+      let videos = document.querySelectorAll('.info-media-video');
+
+      for (let i = 0; i < videos.length; i++) {
+        setupVideo(videos[i]);
+      }
+    }
+
+    function setupVideo(video) {
+      let link = video.querySelector('.info-media-video-link');
+      let media = video.querySelector('.video-media');
+      let button = video.querySelector('.video-btn');
+      let id = parseMediaURL(media);
+
+      video.addEventListener('click', () => {
+        let iframe = createIframe(id);
+
+        link.remove();
+        button.remove();
+        video.appendChild(iframe);
+      });
+
+      link.removeAttribute('href');
+      video.classList.add('video-enabled');
+    }
+
+    function parseMediaURL(media) {
+      let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+      let url = media.src;
+      let match = url.match(regexp);
+
+      return match[1];
+    }
+
+    function createIframe(id) {
+      let iframe = document.createElement('iframe');
+
+      iframe.setAttribute('allowfullscreen', '');
+      iframe.setAttribute('allow', 'autoplay');
+      iframe.setAttribute('src', generateURL(id));
+      iframe.classList.add('video-media');
+
+      return iframe;
+    }
+
+    function generateURL(id) {
+      let query = '?rel=0&showinfo=0&autoplay=1';
+
+      return 'https://www.youtube.com/embed/' + id + query;
+    }
+
+    findVideos();
+  })();
 });
