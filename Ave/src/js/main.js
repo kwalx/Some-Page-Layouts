e24572c15a510e207fb25ecde5e4d3d135e35343
kwalx <alkrivulya@gmail.com>
2019-10-08 01:00:13 +0300

    sticky header

--- Ave/src/js/main.js
+++ Ave/src/js/main.js
@@ -1,5 +1,78 @@
-const btnMenu = document.querySelector('.btn-menu');
+(function() {
+  /**
+   * Sticky header
+   */
+  const navigationContainer = document.querySelector('.section-nav');
+  const banner = document.querySelector('.banner');
+  const navigationContainerHeight = navigationContainer.getBoundingClientRect()
+    .height;
 
-btnMenu.addEventListener('click', () => {
-  document.querySelector('.page-nav-inner').classList.toggle('close');
-});
+  let lastScrollTop = 0;
+  window.addEventListener('scroll', pageOnScroll, false);
+
+  function pageOnScroll(e) {
+    let top = window.pageYOffset;
+
+    if (
+      lastScrollTop > top &&
+      navigationContainer.getBoundingClientRect().top <= 0
+    ) {
+      banner.classList.add('desktop-sticky');
+      navigationContainer.style.top = '-110%';
+      banner.style.paddingTop = `${navigationContainerHeight}px`;
+    } else if (
+      lastScrollTop < top ||
+      navigationContainer.getBoundingClientRect().top > 0
+    ) {
+      banner.classList.add('desktop-sticky');
+      navigationContainer.style.top = '0';
+      banner.style.paddingTop = `${navigationContainerHeight}px`;
+    }
+
+    if (banner.getBoundingClientRect().top >= 0) {
+      banner.classList.remove('desktop-sticky');
+      banner.style.paddingTop = '';
+    }
+
+    lastScrollTop = top;
+  }
+
+  /**
+   * Navigation
+   */
+  const navContainer = document.querySelector('.page-navigation');
+  const btnMenu = document.querySelector('.btn-menu');
+
+  btnMenu.addEventListener('click', () => {
+    navContainer.classList.toggle('overlay');
+    document.querySelector('.page-nav-inner').classList.toggle('close');
+  });
+
+  /**
+   * Home page tabs
+   */
+  const tabLinksItem = document.querySelectorAll('.tabs-navigation__link');
+  const tabContentItem = document.querySelectorAll('.tabs-content');
+
+  tabLinksItem.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      for (let i = 0; i < tabLinksItem.length; i++) {
+        tabLinksItem[i].classList.remove('tabs-navigation__link--active');
+      }
+
+      if (!link.classList.contains('tabs-navigation__link--active')) {
+        link.classList.add('tabs-navigation__link--active');
+      }
+
+      for (let i = 0; i < tabContentItem.length; i++) {
+        tabContentItem[i].classList.remove('tabs-content--active');
+
+        if (link.getAttribute('href') == tabContentItem[i].getAttribute('id')) {
+          tabContentItem[i].classList.add('tabs-content--active');
+        }
+      }
+    });
+  });
+})();
