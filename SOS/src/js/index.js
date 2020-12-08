3b175cfbf9d39bd578422fc72eeb7ae8d9e5bde6
kwalx <alx.krivulya.dev@gmail.com>
2020-12-08 19:57:57 +0200

    added "about" section

--- SOS/src/js/index.js
+++ SOS/src/js/index.js
@@ -56,3 +56,29 @@
     showHidePanel();
   });
 })();
+
+// Swiper slider
+
+(() => {
+  var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 100,
+      stretch: 0,
+      depth: 500,
+      modifier: 1,
+      slideShadows: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    }
+  });
+})();
