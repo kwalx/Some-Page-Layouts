355b824c63d92bd7f6dfc199343b9b63f4e0699f
kwalx <alx.krivulya.dev@gmail.com>
2021-01-04 14:52:32 +0200

    added script for anchor links

--- SOS/src/js/index.js
+++ SOS/src/js/index.js
@@ -2,7 +2,7 @@
 
 (() => {
   const languageButton = document.querySelector('.lang__button');
-  const languageMenu = document.querySelector('#language-menu');
+  const languageMenu = document.querySelector('.lang__list');
 
   languageButton.addEventListener('click', () => {
     let expanded = languageMenu.getAttribute('aria-expanded') == 'false';
@@ -54,6 +54,44 @@
 
   overlay.addEventListener('click', () => {
     showHidePanel();
+  });
+})();
+
+// Anchor scroll
+
+(() => {
+  const anchors = document.querySelectorAll('.nav-link');
+  const scrollSpeed = 0.3;
+
+  anchors.forEach((item) => {
+    item.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const w = window.pageYOffset;
+      const hash = this.href.replace(/[^#]*(.*)/, '$1');
+      const indent = document.querySelector(hash).getBoundingClientRect().top;
+      let start = null;
+
+      requestAnimationFrame(step);
+
+      function step(time) {
+        if (start === null) start = time;
+
+        const progress = time - start;
+        const r =
+          indent < 0
+            ? Math.max(w - progress / scrollSpeed, w + indent)
+            : Math.min(w + progress / scrollSpeed, w + indent);
+
+        window.scrollTo(0, r);
+
+        if (r != w + indent) {
+          requestAnimationFrame(step);
+        } else {
+          location.hash = hash;
+        }
+      }
+    });
   });
 })();
 
